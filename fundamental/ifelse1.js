7377e7ff38cf7be88b0a16367d5f484f22259dbf
Ranvir Singh <ranvir.singh@quantela.com>
2024-12-02 21:23:57 +0530

    loops for of and for in practice done

--- fundamental/ifelse1.js
+++ fundamental/ifelse1.js
@@ -18,100 +18,229 @@
 
 
 // What happens if you forget to put curly braces {} around the block of code inside an if statement? Demonstrate with an example.
-
+// if we forget to put braces then the scope of if will be only one / next line
+// if(2==20)
+//     console.log('if block');
+// console.log('outside if block');
+    
 
 // Write an if statement to check if a variable contains a truthy value.
-// Example:
+// let value = "Hello"; // Check if the value is truthy
+// if(value)
+//     console.log(`Value is not empty`);    
 
-// javascript
-// Copy code
-// let value = "Hello";
-// // Check if the value is truthy
+
 // Create a program to check if a number is greater than 100. If true, print "Large number."
+// let num = 101;
+// if(num>100)
+//     console.log(`large number`);
+    
 
 // Write an if statement to check if a string contains the word "JavaScript".
 // Example Input: "I love JavaScript" → Expected Output: "JavaScript found!"
+// let str = "I love javascript";
+// if(str.includes("javascript"))
+//     console.log("Javascript found!");
+    
 
 // What is the output of this code?
-
-// javascript
-// Copy code
 // let num = 7;
 // if (num % 2 === 0) 
 //     console.log("Even number");
-// Write an if statement to check if a variable is undefined.
-// Example:
+// else 
+//     console.log("Odd number");
 
-// javascript
-// Copy code
+
+// Write an if statement to check if a variable is undefined.
 // let data;
-// // Check if data is undefined
+// if(typeof data === 'undefined')
+//     console.log(`the variable is undefined`);
+
+
 // Create a program that checks if the current hour (using new Date().getHours()) is between 6 and 12. If true, print "Good Morning."
+// let hour = new Date().getHours()
+// if((hour > 5) && (hour < 13) ) {
+//     console.log("Good morning");
+// }
+
+
 
 // 2. If-Else Statements
+// ============================
+
 // Write an if-else statement to determine if a number is even or odd.
 // Example Input: 7 → Expected Output: "Odd number."
+// let num = 7
+// if(num%2===0) {
+//     console.log('Even number');
+// } else {
+//     console.log('Odd number');
+// }
+
 
 // Write an if-else statement to check if a user has enough money to purchase an item.
-// Example:
-
-// javascript
-// Copy code
 // let userMoney = 50;
 // let itemPrice = 40;
-// Write a program to check if a student has passed or failed. (Passing marks are 40 or higher.)
+// if(userMoney >= itemPrice) {
+//     console.log("user has enough money");    
+// }
+// else {
+//     console.log("user has not enough money");
+// }
 
-// Create an if-else statement to categorize a number as positive, negative, or zero.
+
+// Write a program to check if a student has passed or failed. (Passing marks are 40 or higher.)
+// let marks = 39
+// if(marks>= 40) {
+//     console.log("pass");    
+// } else {
+//     console.log('failed');    
+// }
+
 
 // What is the output of the following code?
-
-// javascript
-// Copy code
 // let x = "10";
 // if (x === 10) {
 //     console.log("Strictly Equal");
 // } else {
 //     console.log("Not Strictly Equal");
 // }
+
+
 // Write an if-else program to check if a string is empty or not.
+// let str = "w"
+// if(str.length>0) {
+//     console.log(`string is not empty`);    
+// }
+// else {
+//     console.log(`string is empty`);
+// }
+
 
 // Create a program that determines the grade of a student based on their marks:
-
 // If marks are greater than or equal to 90, print "A."
 // If marks are between 80 and 89, print "B."
 // Otherwise, print "Fail."
+// let marks = 80
+// if(marks>= 90) {
+//     console.log(`Grade A`);
+// }
+// else if((marks>=80) && (marks<90)) {
+//     console.log(`Grade B`);    
+// } else {
+//     console.log(`Fail`);    
+// }
+
+
 // What happens if you use return inside an if-else block? Demonstrate with an example.
+// control will skip the below of return part in if block, it will jump out of if block
+// if(true) {
+//     return
+//     console.log('inside if block');    
+// }
+
 
 // Write an if-else program to determine if the given year is a leap year. (Hint: A year is a leap year if it is divisible by 4 and not divisible by 100, or divisible by 400.)
+// let year = 2025
+// if(year%4===0) {
+//     console.log(`Leap year`);
+// } else {
+//     console.log(`Not leap year`);
+// }
+
 
 // Create an if-else program that checks the type of a variable and prints whether it is a number, string, or boolean.
+// let variable = false
+// if(typeof variable === 'string') {
+//     console.log(`Variable is String`);    
+// }
+// else if(typeof variable === 'number') {
+//     console.log(`Variable is Number`);   
+// }
+// else if(typeof variable === 'boolean') {
+//     console.log(`Variable is Boolean`);    
+// }
+
 
 // 3. Switch Statements
+// =====================
+
 // Write a switch statement to print the day of the week based on a number (1 = Sunday, 2 = Monday, etc.).
+// let day = 2
+// switch (day) {
+//     case 1:
+//         console.log("Sunday");
+//         break;
+//     case 2:
+//         console.log("Monday");
+//         break;
+//     case 3:
+//         console.log("Tuesday");
+//         break;        
+//     default:
+//         console.log("Unknown day");
+//         break;
+// }
+
 
 // Create a switch program to handle different mathematical operations:
-
 // If the user inputs "+", add two numbers.
 // If the user inputs "-", subtract two numbers.
 // If the user inputs any other symbol, print "Invalid operation."
+// let opr = "-"
+// let a = 10, b = 5
+// switch(opr) {
+//   case "+":
+//     console.log(a + b);
+//     break;
+//   case "-":
+//     console.log(a - b);
+//     break;
+//   default:
+//     console.log("Invalid operation");
+//     break;
+// }
+
+
+
 // Write a switch program that checks the value of a fruit name and prints its color.
 // Example:
-
 // "Apple" → "Red"
 // "Banana" → "Yellow"
 // "Orange" → "Orange"
+// let fruit = "apple"
+// switch(fruit) {
+//     case "apple":
+//         console.log("red");
+//         break;
+//     case "banana":
+//         console.log("yellow");
+//         break;
+//     case "orange":
+//         console.log("orange");
+//         break;
+// }
+
+
+
 // What happens if you forget the break keyword in a switch case? Demonstrate with an example.
+// control will not break and it continues to check next case and execute till break statement
+// let num = 1
+// switch(num) {
+//     case 1:
+//         console.log("One");
+//     case 2:
+//         console.log("two");        
+//     case 3:
+//         console.log("three");
+//         break;
+//     case 4:
+//         console.log("three");
+//         break;        
+// }
 
-// Write a switch program to assign grades based on marks:
 
-// 90-100 → "A"
-// 80-89 → "B"
-// 70-79 → "C"
-// Below 70 → "Fail"
 // What is the output of this code?
-
-// javascript
-// Copy code
 // let score = 2;
 // switch (score) {
 //     case 1:
@@ -125,22 +254,76 @@
 //     default:
 //         console.log("Unknown");
 // }
+
+
 // Write a switch statement to determine the type of weather based on input.
 // Example:
-
 // "Sunny" → "Wear sunglasses."
 // "Rainy" → "Carry an umbrella."
 // "Cold" → "Wear a jacket."
+// let word = "sunny"
+// switch(word) {
+//     case "sunny":
+//         console.log("wear sunglasses");
+//         break;
+//     case "rainy":
+//         console.log("carry an umbrella");
+//         break;
+//     case "cold":
+//         console.log("wear a jacket");
+//         break;
+// }
+
 // Can you use expressions in case labels in a switch statement? Show an example.
+// let a = 10, b =5
+// switch(a*b) {
+//     case 50:
+//         console.log(`the product of a and b is 50`);
+//         break;
+//     default:
+//         console.log(`the product is not 50`);
+//         break;
+// }
+
 
 // Create a switch program that categorizes animals into groups:
-
 // "Dog", "Cat" → "Mammals"
 // "Eagle", "Parrot" → "Birds"
 // "Snake", "Crocodile" → "Reptiles"
+// let animal = "parrot"
+// switch(animal) {
+//     case "cat":
+//     case "dog":
+//         console.log("mammals");
+//         break;
+//     case "eagle":
+//     case "parrot":
+//         console.log("birds");
+//         break;
+//     case "snake":
+//     case "crocodile":
+//         console.log("reptiles");
+//         break;                                            
+//     default:
+//         console.log("undefined or invalid");
+//         break;        
+// }
+
+
 // Write a switch program to determine the price of a product based on its name.
 // Example:
-
 // "Milk" → "$5"
 // "Bread" → "$3"
 // "Eggs" → "$2"
+// let product = "bread"
+// switch(product) {
+//     case "milk":
+//         console.log("$5");
+//         break;
+//     case "bread":
+//         console.log("$3");
+//         break;    
+//     case "eggs":
+//         console.log("$2");
+//         break;    
+// }
---
+++ fundamental/loops.js
@@ -0,0 +1,537 @@
+// 1. For Loop
+// =============
+
+// // Write a for loop to print numbers from 1 to 10.
+// for (let index = 1; index <= 10; index++) {
+//     console.log(index);        
+// }
+
+
+// Write a for loop to print all even numbers between 1 and 20.
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index%2===0){
+//         console.log(index);
+//     }
+// }
+
+
+// Use a for loop to calculate the sum of numbers from 1 to 100.
+// let sum = 0
+// for (let index = 1; index <= 100; index++) {
+//     sum += index
+// }
+// console.log(sum);
+
+
+// Write a for loop to print the multiplication table of 5.
+// let num = 5
+// for (let index = 1; index <= 10; index++) {
+//     console.log(`${num} * ${index} = ${num * index}`);
+// }
+
+
+// Write a for loop to reverse a string.
+// Example Input: "JavaScript" → Output: "tpircSavaJ"
+// let str = "Javascript"
+// let revstr = ""
+// for (let index = str.length -1; index >= 0; index--) {
+//     revstr += str[index]
+// }
+// console.log(revstr);
+
+
+
+// Write a for loop to iterate through an array of numbers and print the square of each number.
+// Example: [2, 3, 4] → 4, 9, 16
+// let arr = [2,3,4]
+// let arr1 = []
+// for (const element of arr) {
+//     arr1.push(element * element)
+// }
+// console.log(arr1);
+
+
+// Use a for loop to find the maximum value in an array of numbers.
+// Example: [1, 8, 3, 10] → 10
+
+// let arr = [1, 8, 3, 10, 5]
+// let max = arr[0] // let arr[0] is max
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] > max) {
+//         max = arr[i]
+//     }
+// }
+// console.log(max);
+
+
+// Create a program that stops a for loop when a specific value is found in an array.
+// Example: Stop when the number 7 is found in [2, 4, 7, 10].
+// let arr = [2, 4, 7, 10]
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//     if(arr[i] == 7) {        
+//         break;
+//     }
+// }
+
+
+// Write a for loop that skips odd numbers in a range and prints only even numbers.
+
+// for (let index = 1; index <= 10; index++) {
+//     if(index%2===0) {
+//         console.log(index);        
+//     }
+// }
+
+
+// Create a program that uses nested for loops to print a 3x3 grid of *.
+// Example Output:
+// * * *
+// * * *
+// * * *
+// let str = ''
+// for (let index = 0; index < 3; index++) {
+//     for(let j=0; j<3; j++) {
+//         str += "*"
+//     }
+//     str += "\n"
+// }
+// console.log(str);
+
+
+
+// 2. While Loop
+// ====================
+
+// Write a while loop to print numbers from 1 to 10.
+// let num = 1 ; // init
+// while (num <= 10) {
+//     console.log(num);
+//     num++;   // increment
+// }
+
+
+// Write a while loop to print all numbers divisible by 5 from 1 to 50.
+// let num = 1;
+// while(num <= 50) {
+//     if(num%5===0) {
+//         console.log(num);        
+//     }
+//     num ++
+// } 
+
+
+// Use a while loop to calculate the factorial of a number.
+// Example: 5! = 5 × 4 × 3 × 2 × 1 = 120
+// let num = 5;
+// let factorial = 1;
+// while(num>0) {
+//     factorial *= num;
+//     num--;
+// }
+// console.log(factorial)
+
+
+// Write a while loop to print the digits of a number in reverse order.
+// Example Input: 12345 → Output: 54321
+
+// let digits = 12345
+// let digitsStr = digits.toString()
+// let digitsrev = ''
+// let length = digitsStr.length - 1 // string length -1 because index starts from 0
+// while(length>=0) {
+//     digitsrev += digitsStr[length]
+//     length--;
+// }
+// console.log(digitsrev);
+
+
+// Use a while loop to find the sum of digits of a number.
+// Example Input: 123 → Output: 6
+// let sum = 0, num = 123
+// let numstr = num.toString()
+// let maxlength = numstr.length
+// let i = 0
+// while (i < maxlength) {
+//     sum += parseInt(numstr[i]) // convert string to number
+//     i++
+// }
+// console.log(sum);
+
+
+// Write a while loop to keep generating random numbers between 1 and 10 until the number 7 is generated.
+// while (true) {
+//     let randomnum = Math.floor(Math.random() * 10) + 1
+//     console.log(`generated number is ${randomnum}`);
+//     if(randomnum===7){
+//         console.log(`number ${randomnum} is generated, exiting the loop`);
+//         break;
+//     }
+// }
+
+
+// Use a while loop to remove elements from an array one by one until it is empty.
+// let arr = [1,2,3,4]
+// while(arr.length > 0) {
+//     arr.pop()
+// }
+// console.log(arr);
+
+
+
+// Write a while loop that keeps prompting the user for input until they type "exit".
+// let input = "exit"
+// while(true) {
+//     console.log(`user typed ${input}`);
+//     if(input==='exit') {
+//         console.log(`user typed ${input}, so exiting from loop`);
+//         break;
+//     }
+// }
+
+
+// Create a program using a while loop that doubles a number until it is greater than or equal to 1000.
+// Example Input: 5 → Output: 5, 10, 20, 40, 80, 160, 320, 640, 1280
+// let num = 5, numdouble = 1
+// while(numdouble <= 1000) {
+//     numdouble = num * 2
+//     num = numdouble
+//     console.log(numdouble);
+// }
+
+
+// Write a while loop to print all characters of a string, one per line.
+// let str = "ranvir", i = 0
+// while (i < str.length) {
+//     console.log(str[i]);
+//     i++;
+// }
+
+
+
+// 3. Do-While Loop
+// =================
+
+
+// Write a do-while loop to print numbers from 1 to 10.
+// let num = 1
+// do {
+//     console.log(num);
+//     num++;
+// } while (num <= 10);
+
+
+// Write a do-while loop to calculate the sum of numbers entered by the user until they enter 0.
+// let sum = 0;
+// let number;
+// do {
+//   number = Math.floor(Math.random() * 10); // did not add 1 here because we need 0 as well randomly
+//   sum += number;
+// } while (number !== 0);
+// console.log("The sum of the numbers is:", sum);
+
+
+// Use a do-while loop to print all even numbers between 1 and 20.
+// let num = 1
+// do {
+//     if(num%2===0)
+//     console.log(num);
+//     num++;
+// } while (num<=20);
+
+
+// Write a do-while loop to count the number of digits in a number.
+// Example Input: 12345 → Output: 5
+// let n = 123, count = 0
+// do{
+//     n = Math.floor(n/10);
+//     count++;
+// }while(n > 0)
+//     console.log(count);
+
+
+// Write a do-while loop to generate a sequence of numbers where each number is double the previous one, 
+// starting at 1. Stop when the number exceeds 100.
+// let num = 1
+// do {
+//     num = num*2
+//     console.log(num);
+// } while (num < 100);
+
+
+// Write a do-while loop to remove all occurrences of a specific value from an array.
+// let arr = [1,2,3,2,4,5]
+// let removenum = 2
+// let i =0 
+// do {
+//     if(arr[i]==removenum) {
+//         arr.splice(i, 1); // start from index and 2nd param how many element to remove, in this case 1 element should remove
+//     } else {
+//         i++;
+//     }
+// } while (i<arr.length);
+// console.log(arr);
+
+
+// Use a do-while loop to reverse an array without using the reverse() method.
+// Example Input: [1, 2, 3] → Output: [3, 2, 1]
+// let arr = [1,2,3]
+// let i = arr.length-1
+// let arr1 = []
+// // console.log(arr.reverse());
+// do {
+//     arr1.push(arr[i])
+//     i--;
+// } while (i>=0);
+// console.log(arr1);
+
+
+// Write a do-while loop to count the number of vowels in a given string.
+// Example Input: "Hello World" → Output: 3
+// let str = "hello worlda"
+// let vowels = ['a', 'e', 'i', 'o', 'u']
+// let vowelscount = 0
+// let i = 0
+// do {
+//     if(vowels.includes(str[i])){
+//         vowelscount++;
+//     }
+//     i++;
+// } while (i<str.length);
+// console.log(vowelscount);
+
+
+
+// 4. For...Of Loop
+// ====================
+
+// Use a for...of loop to iterate through an array of numbers and print each one.
+// let arr = [2,3,4]
+// for (const element of arr) {
+//     console.log(`${element}`);
+// }
+// Note : const element in arr (this will print index)
+
+// Write a for...of loop to concatenate all elements in an array into a single string.
+// Example Input: ["I", "love", "JavaScript"] → Output: "I love JavaScript"
+// let array = ["I", "love", "JavaScript"];
+// let str = ''
+// for (const element of array) {
+//     str += " "+element
+// }
+// console.log(str.trim());
+
+
+// Use a for...of loop to print all characters in a string, one per line.
+// let str = 'ranvir'
+// for (const element of str.split("")) {
+//     console.log(element);
+// }
+
+
+// Write a for...of loop to calculate the sum of all numbers in an array.
+// let array = [1, 2, 3, 4];
+// let sum = 0;
+// for (const element of array) {
+//     sum += element
+// }
+// console.log(sum);
+
+
+// Use a for...of loop to filter all even numbers from an array and print them.
+// let array = [1, 2, 3, 4];
+// for (const element of array) {
+//     if(element%2===0) {
+//         console.log(element);
+//     }
+// }
+
+
+// Create a for...of loop to iterate through a Map object and print its keys and values.
+// const myMap = new Map();
+// myMap.set('apple', 10);
+// myMap.set('banana', 20);
+// myMap.set('orange', 30);
+
+// for (const [key, value] of myMap) {
+//   console.log(`Key: ${key}, Value: ${value}`);
+// //   console.log(`Key: ${key}, Value: ${key}`);
+// }
+
+
+// Write a for...of loop to find the longest word in an array of strings.
+// Example Input: ["apple", "banana", "cherry"] → Output: "banana"
+// let array = ["apple", "banana", "cherry"];
+// let longest = array[0]
+// for (const element of array) {
+//     if(longest.length < element.length ) {
+//         longest = element;
+//     }
+// }
+// console.log(longest);
+
+
+// Use a for...of loop to iterate through a Set object and print each element.
+// const mySet = new Set([1, 2, 3, 2, 1, 5]);
+// for (const element of mySet) {
+//     console.log(element);
+// }
+
+
+// Write a for...of loop to double all numbers in an array and create a new array.
+// Example Input: [1, 2, 3] → Output: [2, 4, 6]
+// let arr = [1,2,3]
+// let arr1 = []
+// for (const element of arr) {
+//     arr1.push(element*2)
+// }
+// console.log(arr1);
+
+
+// Write a for...of loop to iterate through an array of objects and print a specific property of each object.
+// Example Input: const users = [{ name: "Alice" }, { name: "Bob" }, { name: "Charlie" }];
+// const users = [{ name: "Alice" }, { name: "Bob" }, { name: "Charlie" }];
+// for (const element of users) {
+//     console.log(`${element.name}`);
+// }
+
+
+
+// 5. For...In Loop
+// =================
+
+// Use a for...in loop to iterate through the properties of an object and print each property name and value.
+// const user = { name: "Ranvir", age : 33, city : 'Delhi' };
+// for (const key in user) {
+//     // option 1
+//     console.log(`${key} : ${user[key]}`);
+//     // option 2
+//     // if (Object.prototype.hasOwnProperty.call(user, key)) {
+//     //     const element = user[key];
+//     //     console.log(element);        
+//     // }
+// }
+
+
+// Write a for...in loop to count the number of properties in an object.
+// const user = { name: "Ranvir", age : 33, city : 'Delhi' };
+// let count = 0;
+// for (const key in user) {
+//     count++;
+// }
+// console.log(count);
+
+
+// Create a for...in loop to copy all properties from one object to another.
+// const user = { name: "Ranvir", age : 33, city : 'Delhi' };
+// const user1 = {};
+// for (const key in user) {
+//     user1[key] = user[key];
+// }
+// console.log(user1);
+
+
+// Write a for...in loop to check if a specific property exists in an object.
+// Example: Check if "age" exists in { name: "Alice", age: 25 }.
+// const user = { name: "Ranvir", age : 33, city : 'Delhi' };
+// const keytocheck = 'aged'
+// let isExists = false;
+
+// for (const key in user) {
+//     if(key === keytocheck){
+//         isExists = true
+//     }
+// }
+// if(isExists) {
+//     console.log(`given key = ${keytocheck} and it exists in object`);
+// } else {
+//     console.log(`given key = ${keytocheck} and it does not exists in object`);
+// }
+
+
+// Use a for...in loop to iterate through the indices of an array and print each index and its corresponding value.
+// let arr = ["apple", "banana", "grapes"];
+// for (const key in arr) {
+//     console.log(arr[key]);
+// }
+
+
+// Write a for...in loop to convert all string values in an object to uppercase.
+// Example Input: { name: "Alice", city: "New York" } → Output: { name: "ALICE", city: "NEW YORK" }
+// const user = { name: "Alice", city: "New York" };
+// const user1 = {};
+// for (const key in user) {
+//     user[key] = user[key].toUpperCase();   
+// }
+// console.log(user);
+
+
+// Use a for...in loop to calculate the total of all numeric properties in an object.
+// Example: { a: 5, b: 10, c: "hello" } → Output: 15
+// const user = { a: 5, b: 10, c: "hello" };
+// let sum = 0;
+// for (const key in user) {
+//     if(typeof user[key]=== 'number'){
+//         sum += user[key];
+//     }
+// }
+// console.log(sum);
+
+
+// Write a for...in loop to count how many times each character appears in a string.
+// Example Input: "hello" → Output: { h: 1, e: 1, l: 2, o: 1 }
+
+// function countCharacterOccurrences(str) {
+//     const charCount = {};
+//     for (const char of str) {
+//         charCount[char] = (charCount[char] || 0) + 1;
+//     }
+//     return charCount;
+// }
+
+// const inputString = "helloo";
+// const characterCounts = countCharacterOccurrences(inputString);
+// console.log(characterCounts);
+
+
+// Use a for...in loop to iterate through an object and remove all properties with a value of null or undefined.
+// let e;
+// let object = { a: 5, b: 10, c: "hello", d : null, e }
+// for (const key in object) {
+//     if((object[key] === null) || (object[key] === undefined)) {
+//         delete object[key];
+//     }
+// }
+// console.log(object);
+
+
+// Write a for...in loop to iterate through a nested object and print all property names and values.
+// Example Input: const obj = { name: "Alice", details: { age: 25, city: "New York" } };
+// const obj = { name: "Alice", details: { age: 25, city: "New York" } };
+// let obj2 = {}
+// for(const key in obj) {
+//     if(typeof obj[key] === 'object'){
+//         obj2 = obj[key];
+//         for(const k in obj2){
+//             console.log(`key=${k} : value=${obj2[k]}`);
+//         }
+//     } else {
+//         console.log(`outer key=${key} : value=${obj[key]}`);
+//     }
+// }
+
+// using recursive function
+const nestedObject = { name: "Alice", details: { age: 25, city: "New York" } };
+
+// function printObject(obj) {
+//     for (const key in obj) {
+//         if (typeof obj[key] === 'object') {
+//             console.log(`${key}:`);
+//             printObject(obj[key]); // Recursively print nested objects
+//         } else {
+//             console.log(`${key}: ${obj[key]}`);
+//         }
+//     }
+// }
+// printObject(nestedObject);
